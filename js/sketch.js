d5558413d827ef6e2a11fa946a4413a52bc4f1da
jadenthinks <116662900+jadenthinks@users.noreply.github.com>
2024-03-13 19:59:07 +0800

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -6,20 +6,29 @@ let n = 10;
 let oX, oY; // Orca X and Y
 let oSX = 2,
   oSY = 2; // Orca Speed
-let oS = 100;
+let oS = 100; //size
+let gF = 50; // Growth speed
+let cs = []; // Circles
 
 function setup() {
-let mycanvas=createCanvas(800, 500);
+    let mycanvas=createCanvas(800, 500);
     mycanvas.parent("p5-canvas-container")
   oX = width / 2;
   oY = height / 2;
+  // Initialize circles
+  for (let i = 0; i < 10; i++) {
+    cs.push({
+      x: random(width),
+      y: random(height),
+      s: random(10, 50),
+    });}
   for (i = 0; i < n; i++) {
     s[i] = 100;
     x[i] = random(width);
     y[i] = random(height);
     speedS[i] = random(0.01, 0.05);
   }
-  background("rgb(48,120,163)");
+  background("black");
   for (let i = n / 2; i < width; i += n) {
     for (let j = n / 2; j < height; j += n) {
       fill(random(0, 10), random(0, 10), random(10, 45));
@@ -34,14 +43,29 @@ function draw() {
   move();
   oX += oSX;
   oY += oSY;
-  if (oX > width - oS / 3 || oX < oS / 3) oSX *= -1;
-  if (oY > height - oS / 3 || oY < oS / 3) oSY *= -1;
+  if (oX >= width - oS / 3 || oX < oS / 3) oSX *= -1;
+  if (oY >= height - oS / 3 || oY < oS / 3) oSY *= -1;
   if (dist(mouseX, mouseY, oX, oY) < 100) {
     // Avoid mouse
-    oSX = oX < mouseX ? -abs(oSX) : abs(oSX);
-    oSY = oY < mouseY ? -abs(oSY) : abs(oSY);
+    oSX = oX <= mouseX ? -abs(oSX) : abs(oSX);
+    oSY = oY <= mouseY ? -abs(oSY) : abs(oSY);
   }
   drawOrca(oX, oY, oS);
+  fill(255);
+  textAlign(CENTER);
+  textSize(16);
+  text('Navigate using the mouse to try and click the touch Orca :)', width/2, height - 20);
+  text('Find the space trash, I might grow', width/2, height - 40);
+
+  for (let i = cs.length - 1; i >= 0; i--) { // Eat circles and grow
+    let c = cs[i];
+    if (dist(oX + oS / 6, oY + oS / 6, c.x, c.y) < (oS / 6 + c.s / 2)) {
+      oS += gF;
+      cs.splice(i, 1);
+    }
+  }
+  fill('rgb(0,0,0)');
+  cs.forEach(c => ellipse(c.x, c.y, c.s, c.s)); // Draw circles
 }
 
 function move() {
